30761837aa77336bf306341ea0600184576fa153
giorgimakasarashvili30112001-stack <giorgimakasarashvili30112001@gmail.com>
2026-08-14 15:56:25 +0400

    Update AppShell.tsx
    added class space-y-3 in AppShell.tsx:7

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -4,7 +4,7 @@ import { BottomNav } from "./BottomNav";
 export function AppShell({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen page-grain pb-24">
-      <div className="mx-auto max-w-md px-5">{children}</div>
+      <div className="mx-auto max-w-md px-5 space-y-3">{children}</div>
       <BottomNav />
     </div>
   );
